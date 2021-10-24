70d14441165d3bac4f08c63bcb3bbc80ed94cacd
Nikita Grebennikov <nikitagreb0204@mail.ru>
2021-10-24 11:13:51 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
-function App() {
+export default function App() {
+  const [toggle, setToggle] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      <hr/>
+      <div className="blocks">
+        {toggle && <div className="square blue">{toggle.toString()}</div>}
+      </div>
     </div>
   );
 }
-
-export default App;
