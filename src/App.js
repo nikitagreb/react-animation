cd1e4c5af29e949787076d6784c6f6cb40223393
Nikita Grebennikov <nikitagreb0204@mail.ru>
2021-10-24 11:36:26 +0300

    15.133

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import {Transition} from 'react-transition-group'
 
 export default function App() {
   const [toggle, setToggle] = useState(true)
@@ -8,7 +9,17 @@ export default function App() {
       <button onClick={() => setToggle(!toggle)}>Toggle</button>
       <hr/>
       <div className="blocks">
-        {toggle && <div className="square blue">{toggle.toString()}</div>}
+        <Transition
+          in={toggle}
+          timeout={{
+            enter: 1000,
+            exit: 500
+          }}
+          mountOnEnter
+          unmountOnExit
+        >
+          {state => <div className={`square blue ${state}`}>{state}</div>}
+        </Transition>
       </div>
     </div>
   );
